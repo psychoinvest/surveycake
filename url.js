a6d216379a0ed92cdf479659e8a65a8de7b8e065
psychoinvest <127083501+psychoinvest@users.noreply.github.com>
2023-03-06 15:03:58 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://www.surveycake.com/s/qZgm9',
+	'https://www.surveycake.com/s/dpgvO',
+	'https://www.surveycake.com/s/odgX9'
 ];
